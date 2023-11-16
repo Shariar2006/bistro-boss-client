127f678703e88b23581609490f9595f1544384f8
Shariar <shariarnafis86@gmail.com>
2023-11-16 19:30:11 +0600

    delete user data to database

--- src/Components/Pages/Dashboard/Admin/AllUsers.jsx
+++ src/Components/Pages/Dashboard/Admin/AllUsers.jsx
@@ -1,16 +1,47 @@
 import { FaTrashCan } from "react-icons/fa6";
+import {  FaUsers } from "react-icons/fa";
 import useAxios from "../../../Hooks/useAxios";
 import { useQuery } from "@tanstack/react-query";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
     const axiosSource = useAxios()
-    const {data} = useQuery({
+    const {data, refetch} = useQuery({
         queryKey: ['users'],
         queryFn: async()=>{
             const res = await axiosSource.get('/users');
             return res.data
         }
     })
+
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSource.delete(`/users/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount>0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                              });
+                              refetch()
+                        }
+                    })
+            }
+        });
+    }
+
+
     return (
         <div>
             <div className="w-11/12 mx-auto p-5 ">
@@ -56,8 +87,11 @@ const AllUsers = () => {
                                 <td>
                                     {user?.email}
                                 </td>
+                                <td>
+                                <button onClick={() => { handleDelete(user?._id) }} className="btn btn-ghost btn-lg "><FaUsers /></button>
+                                </td>
                                 <th>
-                                    <button className="btn btn-ghost btn-lg text-red-500"><FaTrashCan /></button>
+                                    <button onClick={() => { handleDelete(user?._id) }} className="btn btn-ghost btn-lg text-red-500"><FaTrashCan /></button>
                                 </th>
                             </tr>)
                         }
